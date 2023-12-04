a47b7182cda512abe5df4ae9f1d3befb80b2b331
rihab91 <rihab.rjab@hotmail.com>
2023-12-04 01:12:36 +0100

    checkState

--- src/App.js
+++ src/App.js
@@ -1,36 +1,13 @@
 
-// import './App.css';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-     
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
 import React, { Component } from 'react'
 
 export default class App extends Component {
 
 state ={
 person :{
-  fullName: 'Neymar',
-  bio: 'A Footballeur',
+  fullName: 'Neymar da Silva Santos',
+  bio: '(born February 5, 1992, Mogi das Cruzes, Brazil) Brazilian football (soccer) player .',
   imgSrc: 'https://www.nicepng.com/png/detail/173-1739512_neymar-png.png', 
   profession: 'Footballeur',
 } ,
@@ -57,16 +34,16 @@ componentWillUnmount(){
         {/* si notre state: shows ==true ,notre person s'affiche */}
         { (this.state.shows ==true )?
        <div >
-       <h1 style={{color: "red"}}>      {this.state.person.fullName}  </h1>
-       <h2 style={{Color: "red"}}>     {this.state.person.bio}       </h2>
-       <img src={this.state.person.imgSrc} alt="Profile" width={'200px'}  />
-       <h2 >     {this.state.person.profession}</h2>
-       <p>Time:{this.state.time}</p>
+       <h1 style={{color: "red", margin: "10px", textAlign: "center" }}>     {this.state.person.fullName}  </h1>
+       <h3 style={{Color: "blue" , margin: "10px", textAlign: "center"}}>     {this.state.person.bio}       </h3>
+       <img src={this.state.person.imgSrc} alt="Profile" width={'200px'} style={{ marginLeft: "650px"  }} />
+       <h2  style={{color: "#193627", margin: "10px", textAlign: "center" }}>     {this.state.person.profession}</h2>
+       <p style={{color: "red", margin: "10px", textAlign: "center" }}>Time:{this.state.time}</p>
        </div> :null 
         }
 
         {/* si bouton =true ,il se change => false et si false devient =>true*/ }
-      <button onClick={()=>{if (this.state.shows==true){this.setState({shows:false})}else {this.setState({shows:true})};console.log(this.state.shows )}}>show me</button>
+      <button style={{color: "green", marginLeft: "730px"}}  onClick={()=>{if (this.state.shows==true){this.setState({shows:false})}else {this.setState({shows:true})};console.log(this.state.shows )}}>Show me</button>
       
       </div>
     )
